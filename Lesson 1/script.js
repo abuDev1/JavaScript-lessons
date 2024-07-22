c810c4b5e2e87ff8c4d8ff953fef78f51cd46d67
Khusein Muslim <khusein1993@gmail.com>
2024-07-22 17:31:23 +0300

    JS lesson 1

---
+++ Lesson 1/script.js
@@ -0,0 +1,98 @@
+// ////Создание переменных////
+
+// let ageMike = 30;
+
+// var ///почти не используется
+// let
+// const 
+
+
+// ///Типы данных
+
+// ///1. Числа (numbers)
+
+// let age = 30;
+// let height = 1.75;
+// let width = 1.001;
+
+// ///2. Строки (string)
+
+// let name = "John";
+// let message = 'Hello, World!';
+
+// ///3. Булевые значения (boolean)
+
+// let isStudent = true;
+// let isTeacher = false;
+
+// //4. undefined и null
+
+// let myVar = null;
+// let myVar2;///(undefined т.к не присвоили значение)
+
+// ///5. object (объекты)
+
+// let person = {
+//     name: "John",
+//     age: 30,
+//     isStudent: true
+// }
+
+// ///6. arrays (массивы) [входит любой тип данных]
+
+// let fruit = ["apple", "bananas", "orange"]
+// let cardNumbers = [2,45345,562,135,466]
+
+
+// ///7. symbol (символ)
+
+// let id = Symbol ("id")
+
+// ///8. Bigint (очень большие числа) [n - указывает на то, что это тип данных bigint ]
+
+// let myBigint = 47585676578569345617869873156n;
+
+
+// //////////////////
+
+// let a = 10;
+// let b = "10";
+// let c = a + b;
+
+// console.log(c)
+
+
+// let g = 20; ///для let можно перезаписывать значаения
+// g = 50;
+
+// console.log(g)
+
+
+// const f = 1920; ///для const нельзя перезаписывать значения
+
+// console.log(f)
+
+
+
+///Последнее задание
+
+let name = prompt("Студент");
+
+console.log("Студент: ", name);
+
+let address = prompt("Адрес");
+
+console.log("Адрес: ", address);
+
+let phone = prompt("Номер телефона");
+
+console.log("Номер телефона: ", phone);
+
+
+
+
+
+
+
+
+
